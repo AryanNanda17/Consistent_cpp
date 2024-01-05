a6de01d8f4cd31b52e268bc86f1d1a63aa96460e
Aryan <nandaaryan823@gmail.com>
2024-01-06 00:01:25 +0530

    daily practice

---
+++ Daily_Practice/Week1_ArraysAndSortingAlgos/Day4/Raspberries.cpp
@@ -0,0 +1,75 @@
+#include <iostream>
+// #include <bits/stdc++.h>
+#include <numeric>
+#include<map>
+#include<algorithm>
+#define ll long long
+#define mod 1000000007
+using namespace std;
+void precal(){
+}
+bool isPrime(int n)
+{
+    // Corner case
+    if (n <= 1)
+        return false;
+ 
+    // Check from 2 to n-1
+    for (int i = 2; i <= n / 2; i++)
+        if (n % i == 0)
+            return false;
+ 
+    return true;
+}
+void solve(){
+    int n,k;
+    cin>>n>>k;
+    vector<int>v(n);
+    for(int i=0;i<n;i++)cin>>v[i];
+    if(k==2 || k==3 || k==5){
+        int mini = INT_MAX;
+        for(int i = 0;i<n;i++){
+            if(v[i]%k==0)mini=0;
+            mini = min(mini, k-(v[i]%k));
+        }
+        cout<<mini<<endl;
+        return;
+    }
+    else{
+        if(n==1){
+            cout<<n-k<<endl;
+            return ;
+        }
+        int count= 0;
+        for(int i =0;i<n;i++){
+            if(v[i]%2==0)count++;
+        }
+        int mini=max(0,2-count);
+        for(int i = 0;i<n;i++){
+            if(v[i]%k==0)mini=0;
+            mini = min(mini, k-(v[i]%k));
+        }
+        cout<<mini<<endl;
+        return;
+    }
+
+}   
+ 
+int main(){
+    #ifndef ONLINE_JUDGE
+            freopen("input.txt","r",stdin);
+            freopen("output.txt","w",stdout);
+    #endif
+    ios::sync_with_stdio(0);
+            cin.tie(0);
+            cout.tie(0);
+            cout<<fixed;
+            // cout<<setprecision(10);
+            precal();
+            int t=1;
+            cin>>t;
+            for(int i=1;i<=t;i++){
+                solve();
+    }
+    return 0;
+}
---
+++ Daily_Practice/Week1_ArraysAndSortingAlgos/Day4/collectingGame.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+// #include <bits/stdc++.h>
+#include <numeric>
+#include<map>
+#include<algorithm>
+#define ll long long
+#define mod 1000000007
+using namespace std;
+void precal(){
+}
+void solve(){
+    int n;
+    cin>>n;
+    vector<pair<ll,ll> >v;
+    for(int i=0;i<n;i++){
+        int a;cin>>a;
+        v.push_back(make_pair(a,i));
+    }
+    sort(v.begin(),v.end());
+    vector<ll> sum(n);
+    sum[0]=v[0].first;
+    for(int i =1;i<n;i++){
+        sum[i]=v[i].first+sum[i-1];
+    }
+    vector<int> output(n);
+    for(int i=n-1;i>=0;i--){
+        if(i==n-1){
+            output[v[i].second]=i;
+            continue;
+        }
+        if(sum[i]>=v[i+1].first){
+            output[v[i].second]=output[v[i+1].second];
+        }
+        else{
+            output[v[i].second]=i;
+        }
+    }
+    for(int i =0;i<n;i++){
+        cout<<output[i]<<" ";
+    }
+    cout<<endl;
+}   
+ 
+int main(){
+//     #ifndef ONLINE_JUDGE
+//             freopen("input.txt","r",stdin);
+//             freopen("output.txt","w",stdout);
+//     #endif
+    ios::sync_with_stdio(0);
+            cin.tie(0);
+            cout.tie(0);
+            cout<<fixed;
+            precal();
+            int t=1;
+            cin>>t;
+            for(int i=1;i<=t;i++){
+                solve();
+    }
+    return 0;
+}
